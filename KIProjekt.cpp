1d40278d5f3e68bd701b882c3173827ab5d7caca
Salome Brack <salomebrack98@gmail.com>
2023-05-14 21:04:41 +0200

    Select File

--- KIProjekt.cpp
+++ KIProjekt.cpp
@@ -1,8 +1,43 @@
 // KIProjekt.cpp
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream input;
+    string filename = "";
+
+    //Datei öffnen:
+    do {
+        cout << "Geben Sie Namen der Datei ein, die gelesen werden soll.\nDateiname: ";
+        cin >> filename;
+
+        input.open(filename + ".txt");
+    }
+    while (!input.is_open());
+
+    //Datei lesen:
+    while (input) {
+        string varName;
+
+        getline(input, varName, '+');
+
+        int varValue;
+        input >> varValue;
+
+        input >> ws;
+
+        if (!input) {
+            break;
+        }
+
+        cout << "'" << varName << "': " << varValue << endl;
+    }
+
+    input.close();
+
+    return 0;
 }
