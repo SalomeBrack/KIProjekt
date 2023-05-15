16caaa1aeba4bb93b0c0f8a33f958c62a0412d09
Salome Brack <salomebrack98@gmail.com>
2023-05-15 12:30:10 +0200

    Alle Zeichen in eine Liste

--- KIProjekt.cpp
+++ KIProjekt.cpp
@@ -3,13 +3,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
+
+vector<char> vect = {};
+
+void parseLine(string line) {
+    int i = 0;
+
+    while (line[i] != ';' && line.length() > i) {
+        vect.push_back(line[i]);
+        i++;
+    }
+}
 
 void readLine(string line) {
     if (line != "" && line.substr(0,2) != "//") {
-        //
+        bool min = true;
 
-        cout << "'" << line << "'" << endl;
+        if (vect.empty()) {
+            if (line.find(":")) {
+                if (line.find("min:")) {
+                    min = true;
+                }
+                else if (line.find("max:")) {
+                    min = false;
+                }
+                else {
+                    //Fehler: min / max fehlt
+                }
+            }
+            else {
+                //Fehler: Erste Zeile legt kein Ziel fest
+            }
+            parseLine(line.substr(line.find(":") + 1, line.length()));
+
+            //Variablen zählen
+        }
+        else {
+            parseLine(line);
+        }
     }
 }
 
@@ -36,6 +69,11 @@ int main()
     }
 
     input.close();
+
+    std::cout << "Vector = { ";
+    for (char n : vect)
+        std::cout << n << ", ";
+    std::cout << "}; \n";
 
     return 0;
 }
