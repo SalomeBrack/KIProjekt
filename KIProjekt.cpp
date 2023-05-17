fd5eeb5b0747715fc6ba291174e3c6a88e2849f2
Salome Brack <salomebrack98@gmail.com>
2023-05-17 15:46:14 +0200

    Parser fertig

--- KIProjekt.cpp
+++ KIProjekt.cpp
@@ -6,43 +6,102 @@
 #include <vector>
 using namespace std;
 
-vector<char> vect = {};
+vector<int> vect = {};
+int numberOfVariables = 0;
 
-void parseLine(string line) {
-    int i = 0;
+void parseLine(string line, int i)
+{
+    bool num = false;
+    bool inVarName = false;
+    bool negative = false;
+    int val = 0;
+    int temp = 0;
 
-    while (line[i] != ';' && line.length() > i) {
-        vect.push_back(line[i]);
+    while (i < line.length())
+    {
+        val = line[i] - 48;
+        
+        if (val < 10 && val >= 0 && !inVarName)
+        {
+            if (!num)
+            {
+                num = true;
+                temp = 0;
+            }
+            temp = temp * 10 + val;
+
+            if (negative)
+            {
+                temp = temp * (-1);
+                negative = false;
+            }
+        }
+        else if (line[i] == '+' || line[i] == '<' || line[i] == '>' || line[i] == '=')
+        {
+            inVarName = false;
+            negative = false;
+        }
+        else if (line[i] == '-')
+        {
+            inVarName = false;
+            negative = true;
+        }
+        else if (num)
+        {
+            vect.push_back(temp);
+            temp = 0;
+            num = false;
+            inVarName = true;
+        }
+
         i++;
     }
 }
 
 void readLine(string line) {
     if (line != "" && line.substr(0,2) != "//") {
-        bool min = true;
+        int minOrMax = 0;
 
-        if (vect.empty()) {
-            if (line.find(":")) {
-                if (line.find("min:")) {
-                    min = true;
+        if (vect.empty())
+        {
+            if (line.find(":"))
+            {
+                if (!line.find("min"))
+                {
+                    minOrMax = 0;
                 }
-                else if (line.find("max:")) {
-                    min = false;
-                }
-                else {
-                    //Fehler: min / max fehlt
+                else if (!line.find("max"))
+                {
+                    minOrMax = 1;
                 }
             }
-            else {
-                //Fehler: Erste Zeile legt kein Ziel fest
-            }
-            parseLine(line.substr(line.find(":") + 1, line.length()));
 
-            //Variablen zählen
+            vect.push_back(minOrMax);
+            parseLine(line, line.find(":") + 1);
+            numberOfVariables = vect.size() - 1;
         }
-        else {
-            parseLine(line);
+        else
+        {
+            parseLine(line, 0);
         }
+    }
+}
+
+void printNumbers()
+{
+    int i = 1;
+
+    if (vect[0] == 0) { cout << "min:  "; } else { cout << "max:  "; }
+
+    for (int j = 0; j < vect.size() / (numberOfVariables + 1); j++) {
+        while (i <= numberOfVariables)
+        {
+            cout << vect[j * (numberOfVariables + 1) + i] << "  ";
+            i++;
+        }
+
+        cout << endl;
+        i = 0;
     }
 }
 
@@ -52,7 +111,8 @@ int main()
     string filename = "";
 
     //Datei öffnen:
-    do {
+    do
+    {
         cout << "Geben Sie Namen der Datei ein, die gelesen werden soll.\nDateiname: ";
         cin >> filename;
 
@@ -63,17 +123,15 @@ int main()
     //Datei lesen:
     string line;
 
-    while (input) {
+    while (input)
+    {
         getline(input, line);
         readLine(line);
     }
 
     input.close();
 
-    std::cout << "Vector = { ";
-    for (char n : vect)
-        std::cout << n << ", ";
-    std::cout << "}; \n";
+    printNumbers();
 
     return 0;
 }
